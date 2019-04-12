c64477c278a8b9ea587c66b1aafb454446c22cba
Lazar1389 <37537586+Lazar1389@users.noreply.github.com>
2019-04-12 14:15:10 +0200

    Add files via upload

---
+++ EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+
+        // Inject IEmployeeRepository using Constructor Injection
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        // Retrieve employee name and return
+        public ViewResult Index()
+        {
+            // retrieve all the employees
+            var model = _employeeRepository.GetAllEmployees();
+            // Pass the list of employees to the view
+            return View(model);
+        }
+
+        public ViewResult Details(int id)
+        {
+
+            Employee Employee = _employeeRepository.GetEmployee(id);
+     
+            // Pass the ViewModel object to the View() helper method
+            return View(Employee);
+        }
+        [HttpGet]
+        public ViewResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                Employee newEmployee = _employeeRepository.Add(employee);
+                return RedirectToAction("details", new { id = newEmployee.Id });
+            }
+            return View();
+        }
+
+    }
+}
